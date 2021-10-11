956e03fde1b0bfc1413aaf1e7991e0aeff0aa610
Jeramir1 <91283211+Jeramir1@users.noreply.github.com>
2021-10-11 17:50:27 -0400

    Add files via upload

---
+++ treasureisland.py
@@ -0,0 +1,41 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+crossroad = input("You are at a crossroad, where do you want to go? Type 'left' or 'right'")
+if crossroad == 'right':
+  print("You fell into a pit and died, game over")
+elif crossroad == 'left':
+  lake= input ("you came into a lake, would you like to swim accross, or wait for a boat? Type 'swim' or 'wait'")
+  if lake == 'swim':
+    print ("You've drowned, game over")
+  elif lake == 'wait':
+    door = input ("a boat has come and brought you to shore, you see 3 doors, a red door, a blue door and a yellow door, which one do you choose? Type 'red', 'blue', or 'yellow'")
+    if door == 'red':
+      print("You have found an inferno inside and burned to a crisp, game over")
+    elif door == 'blue':
+      print("you fell into an infinity pit and died, game over")
+    elif door == 'yellow':
+      print("You found the treause and won the game!")
